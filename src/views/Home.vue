20a48b9e98f33dbb1a9fe1be62474bc0d4751c16
Karolis Vaitkevicius <karolis.vaitkevicius@77diamonds.com>
2021-09-30 23:43:13 +0000

    basic homepage testing publish

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="home">
+		<h2>Homepage</h2>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { reactive } from "vue";
+
+export default {
+	name: "Home",
+
+	setup() {
+		const state = reactive({
+		});
+
+		return {
+			state,
+		};
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.home{
+	background: black;
+	color: blanchedalmond;
+}
+</style>
