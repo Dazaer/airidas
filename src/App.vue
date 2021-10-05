aa82db2709b776cb260ae2f0853ccc994b65f585
Karolis Vaitkevicius <karolis.vaitkevicius@77diamonds.com>
2021-10-05 22:04:56 +0000

    add possibility to override theme

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
-	<div id="nav">
-		<router-link to="/">Home</router-link>|
-		<router-link to="/about">About</router-link>
-	</div>
-	<router-view />
-	<div>
-		<HelloWorld location="Airidas.net"></HelloWorld>
-	</div>
+	<n-config-provider :theme-overrides="themeOverrides">
+		<div id="nav">
+			<router-link to="/">Home</router-link>|
+			<router-link to="/about">About</router-link>
+		</div>
+		<router-view />
+
+		<div>
+			<n-button>naive-ui</n-button>
+			<HelloWorld location="Airidas.net"></HelloWorld>
+		</div>
+	</n-config-provider>
 </template>
 
 <script lang="ts" setup>
+import { GlobalThemeOverrides } from "naive-ui";
 import HelloWorld from "./components/HelloWorld.vue";
+
+const themeOverrides: GlobalThemeOverrides = {
+	common: {
+		primaryColor: '#FFFF00',
+		textColorBase: '#FFFF00',
+		actionColor: '#FFFF00',
+		bodyColor: '#FFFF00',
+		textColor1: '#FFFF00',
+		//textColor2: '#FFFF00', //Color of text inside button
+		textColor3: '#FFFF00',
+		//primaryColorHover: '#FFFF00', // On primary button hover
+		hoverColor: '#FF0000',
+	},
+	Button: {
+		//textColor: '#FFFF00'
+	},
+	Select: {
+		/*
+		peers: {
+			InternalSelection: {
+				textColor: '#FF0000'
+			}
+		}
+		*/
+	},
+	// ...
+}
+
 </script>
 
 <style lang="scss">
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import {
+  create as createNaiveUi,
+  // components
+  NButton,
+	NConfigProvider
+} from 'naive-ui'
 
-export const vueApp = createApp(App).use(router).mount("#app");
+const naive = createNaiveUi({
+  components: [
+		NConfigProvider,
+		NButton, 
+	],
+})
+
+export const vueApp = createApp(App)
+.use(naive)
+.use(router)
+.mount("#app");
