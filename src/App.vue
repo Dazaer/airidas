e18119128ad024f5ee123e7fe8e2a5c4840055e2
Karolis Vaitkevicius <karolis.vaitkevicius@77diamonds.com>
2021-11-14 15:10:12 -0100

    navbar properly formatted

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,28 @@
 <template>
-		<p-menubar :model="menu">
-			<template #start>
-				<router-link to="/">
-					<p-button class="p-button-text p-button-text mr-4">Airidas.net</p-button>
-				</router-link>
-			</template>
-			<!-- <template #end></template> -->
-		</p-menubar>
-
+		<MainNavbar></MainNavbar>
 		<router-view />
 
-		<div>
-			<p-button label="PrimeVue" icon="pi pi-user"></p-button>
-			<button>regular</button>
+	<!--
+	<div>
+		<p-button label="PrimeVue" icon="pi pi-user"></p-button>
 			<HelloWorld location="Airidas.net"></HelloWorld>
 		</div>
+	-->
 </template>
 
 <script lang="ts" setup>
-import { RouterLink } from "vue-router";
-import { ref } from "vue"
-import HelloWorld from "./components/HelloWorld.vue";
-
-const menu = ref([
-	/*
-	{
-		label: 'Home',
-		icon: "pi pi-home",
-		to: "/"
-	},
-	*/
-	{
-		label: 'About',
-		to: { name: "About" }
-	},
-	{
-		label: 'Feature Request',
-		//to: { name: "feature-request" }
-	},
-	{
-		label: 'Login',
-		//to: { name: "login" },
-		disabled: true,
-	},
-	{
-		label: 'Profile',
-		items: [
-			{ label: "Settings", icon: "pi pi-fw pi-cog" /*to: "/settings"*/ },
-			{ label: "Logout", icon: "pi pi-fw pi-power-off", command: function () { alert("You have logged out") } }
-		]
-	},
-]);
+//import HelloWorld from "./components/HelloWorld.vue"
+import MainNavbar from "./components/MainNavbar.vue"
 </script>
 
 <style lang="scss">
 #app {
-	//font-family: "Butler", 'Segoe UI', 'Lucida Sans Unicode', Verdana;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	//	font-size: medium;
 
+	//text-align: center;
+	//font-family: "Butler", 'Segoe UI', 'Lucida Sans Unicode', Verdana;
+	//font-size: medium;
 	//height: 100%;
-}
-
-#nav {
-	padding: 30px;
-
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-
-		&.router-link-exact-active {
-			color: #42b983;
-		}
-	}
 }
 </style>
---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,59 @@
+<template>
+	<p-menubar :model="menu">
+		<template #start>
+			<router-link to="/">
+				<p-button class="p-button-text p-button-text mr-4 home-button">Airidas.net</p-button>
+			</router-link>
+		</template>
+		<!-- <template #end></template> -->
+	</p-menubar>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+const menu = ref([
+	/*
+	{
+		label: 'Home',
+		icon: "pi pi-home",
+		to: "/"
+	},
+	*/
+	{
+		label: "About",
+		to: { name: "About" }
+	},
+	{
+		label: "Feature Request",
+		//to: { name: "feature-request" }
+	},
+	{
+		label: "Login",
+		//to: { name: "login" },
+		disabled: true,
+	},
+	{
+		label: "Profile",
+		items: [
+			{ label: "Settings", icon: "pi pi-fw pi-cog" /*to: "/settings"*/ },
+			{ label: "Logout", icon: "pi pi-fw pi-power-off", command: function () { alert("You have logged out") } }
+		]
+	},
+])
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+.home-button {
+	font-family: "OptimusPrinceps", "Butler";
+}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+</style>
