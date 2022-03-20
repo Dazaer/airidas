575087e3ec42f09082806bf26babd319abdea681
Karolis Vaitkevicius <karolis.vaitkevicius@77diamonds.com>
2022-03-20 16:37:51 +0000

    ADD: recipes page

---
+++ src/views/Recipes.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="grid grid-nogutter col-12 md:col-10 md:col-offset-1 lg:col-8 lg:col-offset-2">
+		<section class="grid grid-nogutter col-12 center">
+			<h1 class="col-12">Recipes</h1>
+
+			<p class="col-12">View and add recipes!</p>
+		</section>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, Ref, ref } from "vue";
+import { useConfirm } from "primevue/useconfirm";
+import { useToast } from "primevue/usetoast";
+
+/* ------------------- Properties ----------------- */
+const toast = useToast();
+const confirm = useConfirm();
+
+
+/* ------------------- Methods ----------------- */
+
+
+
+/* ------------------- Lifecycle ----------------- */
+
+
+</script>
+
+<style scoped lang="scss">
+</style>
