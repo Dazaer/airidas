91f1683a99771520df9c0784156ea496ecba9cdf
Karolis Vaitkevicius <karolis.vaitkevicius@77diamonds.com>
2022-04-07 23:02:15 +0100

    ADD: BookReader, PencilAlt, Trash

--- src/libraries/font-awesome.ts
+++ src/libraries/font-awesome.ts
@@ -2,21 +2,24 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 //import { dom } from "@fortawesome/fontawesome-svg-core";
 //import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faSpinner, faMusic, faQuestionCircle, faQuestion, faUtensils, faFlask, faCamera, faCheck, faBook, faDesktop } from "@fortawesome/free-solid-svg-icons";
+import { faSpinner, faMusic, faQuestionCircle, faQuestion, faUtensils, faFlask, faCamera, faCheck, faBook, faDesktop, faBookReader, faPencilAlt, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons/faFacebook";
 
 library.add(
 	//Solids (fas)
 	faBook,
+	faBookReader,
 	faCamera,
 	faCheck,
 	faDesktop,
 	faFlask,
 	faMusic,
+	faPencilAlt,
 	faQuestion,
 	faQuestionCircle,
 	faSpinner,
+	faTrash,
 	faUtensils,
 	//Brands (fab)
 	faLinkedin,
