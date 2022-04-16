aa7a9b31b9c6dcb231b2150a2eacbc6656560d59
Karolis Vaitkevicius <karolis.vaitkevicius@77diamonds.com>
2022-04-16 21:43:20 +0100

    ADD: Recipes RecipeTags CRUD

--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -1,12 +1,14 @@
+import RecipeTag from '@/models/RecipeTag';
 import Recipe from "@/models/Recipe";
 import { documentSnapshotToModel, querySnapshotToModelArray } from "@/utilities/firebase/firestoreModelConverter";
 import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, getFirestore, orderBy, OrderByDirection, query, QueryConstraint, updateDoc } from "firebase/firestore"
 import { addDefaultValueForFieldInCollection, CollectionFieldUpdateModel } from "@/utilities/firebase/firestoreFunctions";
+import RecipeRecipeTagController from "./RecipeRecipeTagController";
 
 export default class RecipeController {
 
 	private readonly db = getFirestore();
-	private readonly COLLECTION_PATH = 'recipes'
+	public static readonly COLLECTION_PATH = 'recipes'
 
 	private _orderByField: OrderByField | null = null
 
@@ -18,23 +20,23 @@ export default class RecipeController {
 	}
 
 	async get(id: string): Promise<Recipe> {
-		const docRef = doc(this.db, this.COLLECTION_PATH, id).withConverter(Recipe.firestoreConverter);
+		const docRef = doc(this.db, RecipeController.COLLECTION_PATH, id).withConverter(Recipe.firestoreConverter);
 		const documentSnapshot = await getDoc(docRef)
+		
 		let model = documentSnapshotToModel<Recipe>(Recipe, documentSnapshot, "id")
-
-		/*
-		This is where we would get the extra info about each recipeTag from the collection
-		*/
-
 		if (model == null) {
 			model = new Recipe()
 		}
+		
+		const recipeRecipeTagController = new RecipeRecipeTagController(id);
+		const recipeTags: RecipeTag[] = await recipeRecipeTagController.getAll();
+		model.tags = recipeTags;
 
 		return model
 	}
 
 	async getAll(): Promise<Recipe[]> {
-		const collectionRef = collection(this.db, this.COLLECTION_PATH).withConverter(Recipe.firestoreConverter)
+		const collectionRef = collection(this.db, RecipeController.COLLECTION_PATH).withConverter(Recipe.firestoreConverter)
 
 		const queryConstraints: QueryConstraint[] = []
 		if (this._orderByField != null) {
@@ -54,22 +56,42 @@ export default class RecipeController {
 	}
 
 	async add(recipe: Recipe): Promise<any> {
-		const collectionRef = collection(this.db, this.COLLECTION_PATH).withConverter(Recipe.firestoreConverter);
-		return addDoc(collectionRef, recipe);
+		const collectionRef = collection(this.db, RecipeController.COLLECTION_PATH).withConverter(Recipe.firestoreConverter);
+
+		const recipeRecipeTagController = new RecipeRecipeTagController(recipe.id);
+		await addDoc(collectionRef, recipe);
+
+		const promises:Promise<any>[] = []
+		recipe.tags.forEach(tag => {
+			promises.push(recipeRecipeTagController.add(tag))
+		})
+
+		return Promise.all(promises)
 	}
 
 	async update(recipe: Recipe): Promise<any> {
-		const docRef = doc(this.db, this.COLLECTION_PATH, recipe.id).withConverter(Recipe.firestoreConverter);
+		const docRef = doc(this.db, RecipeController.COLLECTION_PATH, recipe.id).withConverter(Recipe.firestoreConverter);
+		const recipeRecipeTagController = new RecipeRecipeTagController(recipe.id);
+
+		const updatePromises:Promise<any>[] = []
+		recipe.tags.forEach(tag => {
+			updatePromises.push(recipeRecipeTagController.update(tag))
+		})
+
+		await Promise.all(updatePromises)
 		return updateDoc(docRef, Recipe.updateToFirestore(recipe))
 	}
 
 	async delete(recipeId: string): Promise<any> {
-		const docRef = doc(this.db, this.COLLECTION_PATH, recipeId);
+		const docRef = doc(this.db, RecipeController.COLLECTION_PATH, recipeId).withConverter(Recipe.firestoreConverter);
+		const recipeRecipeTagController = new RecipeRecipeTagController(recipeId);
+
+		recipeRecipeTagController.deleteAll()
 		return deleteDoc(docRef);
 	}
 
 	updateField<K extends keyof Recipe>(field: K, value: Recipe[typeof field]) {
-		const collectionRef = collection(this.db, this.COLLECTION_PATH).withConverter(Recipe.firestoreConverter);
+		const collectionRef = collection(this.db, RecipeController.COLLECTION_PATH).withConverter(Recipe.firestoreConverter);
 		const updateFieldModel = new CollectionFieldUpdateModel(collectionRef, field, value)
 		addDefaultValueForFieldInCollection(updateFieldModel)
 	}
---
+++ src/controllers/RecipeRecipeTagController.ts
@@ -0,0 +1,81 @@
+import { documentSnapshotToModel, querySnapshotToModelArray } from "@/utilities/firebase/firestoreModelConverter";
+import { addDoc, collection, deleteDoc, doc, DocumentData, getDoc, getDocs, getFirestore, limit, orderBy, Query, query, QueryConstraint, updateDoc, writeBatch } from "firebase/firestore"
+import RecipeTag from "@/models/RecipeTag";
+import RecipeController from "./RecipeController";
+import { deleteQueryBatch } from "@/utilities/firebase/firestoreFunctions";
+
+/**
+ * Yes, this should not actually exist as we do not need a collection for simple RecipeTags. We could hold them in an array of a Recipe as we're currently doing.
+ * However, this is meant for practice and testing.
+ */
+export default class RecipeRecipeTagController {
+
+	private readonly db = getFirestore();
+	public static readonly COLLECTION_PATH = 'recipe-tags'
+	private readonly modelIdentifierName = "id"
+	private recipeId: string = ""
+	
+	public get collectionPath() : string {
+		return `${RecipeController.COLLECTION_PATH}/${this.recipeId}/${RecipeRecipeTagController.COLLECTION_PATH}`
+	}
+
+	constructor(recipeId: string) {
+		this.recipeId = recipeId;
+	}
+
+	async get(id: string): Promise<RecipeTag> {
+		const docRef = doc(this.db, this.collectionPath, id).withConverter(RecipeTag.firestoreConverter);
+		const documentSnapshot = await getDoc(docRef)
+		let model = documentSnapshotToModel<RecipeTag>(RecipeTag, documentSnapshot, "id")
+
+		if (model == null) {
+			model = new RecipeTag()
+		}
+
+		return model
+	}
+
+	async getAll(): Promise<RecipeTag[]> {
+		const collectionRef = collection(this.db, this.collectionPath).withConverter(RecipeTag.firestoreConverter)
+		const queryConstraints: QueryConstraint[] = []
+
+		const dbQuery = query(collectionRef, ...queryConstraints)
+		const querySnapshot = await getDocs(dbQuery)
+
+		const models = querySnapshotToModelArray<RecipeTag>(RecipeTag, querySnapshot, this.modelIdentifierName)
+
+		models.map(model => {
+			return model;
+		})
+
+		return models
+	}
+
+	async add(recipeTag: RecipeTag): Promise<any> {
+		const collectionRef = collection(this.db, this.collectionPath).withConverter(RecipeTag.firestoreConverter);
+		return addDoc(collectionRef, recipeTag);
+	}
+
+	async update(recipeTag: RecipeTag): Promise<any> {
+		const docRef = doc(this.db, this.collectionPath, recipeTag.id).withConverter(RecipeTag.firestoreConverter);
+		return updateDoc(docRef, RecipeTag.updateToFirestore(recipeTag))
+	}
+
+	async delete(recipeTagId: string): Promise<any> {
+		const docRef = doc(this.db, this.collectionPath, recipeTagId);
+		return deleteDoc(docRef);
+	}
+
+	async deleteAll() {
+		const batchSize:number = 50
+		const collectionRef = collection(this.db, this.collectionPath);
+		const queryConstraints: QueryConstraint[] = [orderBy(this.modelIdentifierName), limit(batchSize)]
+		const dbQuery = query(collectionRef, ...queryConstraints)
+	
+		return new Promise((resolve, reject) => {
+			deleteQueryBatch(this.db, dbQuery, resolve).catch(reject);
+		});
+	}
+
+}
+
