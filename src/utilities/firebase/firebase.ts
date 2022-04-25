4d6d0213e57bede5cb60e5dc24c5b7e2536ec746
Karolis Vaitkevicius <karolis.vaitkevicius@77diamonds.com>
2022-04-25 13:21:55 +0100

    UPDATE: firebase config to use env.local

--- src/utilities/firebase/firebase.ts
+++ src/utilities/firebase/firebase.ts
@@ -1,16 +1,39 @@
+import { ProjectConfig } from './../../../project.config';
 import {initializeApp} from "firebase/app"
 
+/*
+import { getAnalytics } from "firebase/analytics";
+*/
+
 const firebaseConfig = {
-  apiKey: "",
-  projectId: "",
-  authDomain: "",
-	databaseURL: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY_PROD,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID_PROD,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN_PROD,
+	databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL_PROD,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET_PROD,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID_PROD,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID_PROD,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID_PROD
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+
+const devFirebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY_DEV,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID_DEV,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN_DEV,
+	//databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL_DEV,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET_DEV,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID_DEV,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID_DEV,
+  //measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID_DEV
+};
+
+const firebaseApp = initializeApp(ProjectConfig.isProductionEnv ? firebaseConfig : devFirebaseConfig);
 
 export default firebaseApp;
+
+/*
+//const analytics = getAnalytics(firebaseApp);
+firebase.initializeApp(firebaseConfig)
+export default firebase;
+*/
