ae71b8cbf1686fac2941c66173342196daef312c
Karolis Vaitkevicius <karolis.vaitkevicius@77diamonds.com>
2022-05-16 00:43:08 +0100

    ADD: Description for currentEnvironment

--- project.config.ts
+++ project.config.ts
@@ -4,6 +4,7 @@ enum ProjectEnvironments {
 	Development,
 }
 export class ProjectConfig {
+	/** IMPORTANT: Change the current environment using one of ProjectEnvironments depending on which one should be used. */
 	public static readonly currentEnvironment: ProjectEnvironments = ProjectEnvironments.Production
 	public static isProductionEnv: boolean = ProjectConfig.currentEnvironment === ProjectEnvironments.Production
 	public static isDebugging: boolean = true
