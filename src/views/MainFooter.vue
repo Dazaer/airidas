b3ab593ccfcc8deb531ab0cd72d5f8cfd70e8549
Karolis Vaitkevicius <karolis.vaitkevicius@77diamonds.com>
2022-11-18 18:07:05 -0100

    REMOVE: Facebook from footer

--- src/views/MainFooter.vue
+++ src/views/MainFooter.vue
@@ -1,12 +1,13 @@
 
 <template>
 	<footer class="flex align-self-end justify-content-center bottom-0 w-full footer p-3">
-		<p-button
+
+		<!-- <p-button
 			@click="openUrl('https://www.facebook.com/Airidas.Dapkevicius')"
 			class="mx-1 p-button-rounded p-button-primary p-button-outlined"
 		>
 			<fa :icon="['fab', 'facebook']"></fa>
-		</p-button>
+		</p-button> -->
 
 		<p-button
 			@click="openUrl('https://www.linkedin.com/in/airidas-dapkevicius-69411723/')"
